82daf529735332f43e00ad1e4ee87c3057a2fe51
t.takahashi <takahashi.developer@gmail.com>
2020-06-20 00:46:46 +0900

    add kinesis name config

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,7 +6,7 @@ PROJECT_NAME="aws-kinesis-sample"
 STACK_NAME="${PROJECT_NAME}-backend"
 ARTIFACT_BUCKET_NAME="iam326.${PROJECT_NAME}-artifact"
 
-aws s3 mb "s3://${ARTIFACT_BUCKET_NAME}"
+# aws s3 mb "s3://${ARTIFACT_BUCKET_NAME}"
 
 sam package \
   --output-template-file packaged.yaml \
@@ -15,6 +15,8 @@ sam package \
 sam deploy \
   --template-file packaged.yaml \
   --stack-name ${STACK_NAME} \
-  --capabilities CAPABILITY_IAM
+  --capabilities CAPABILITY_IAM \
+  --parameter-overrides \
+    NamePrefix=${PROJECT_NAME}
 
 rm packaged.yaml
